a2bc2bf603b6d02d60f074129ed5e22030419d3a
preethititus <preethititus003@gmail.com>
2026-08-10 17:22:24 +0530

    Add Playwright project (#3)
    * Add Playwright project
    
    * Address PR review comments

---
+++ tests/homework3.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('Verify SauceDemo title and URL', async ({ page }) => {
+
+  await page.goto('https://saucelabs.com/');
+
+
+  await expect(page).toHaveTitle('Sauce Labs: AI-Unified Release Assurance Platform');
+
+
+  await expect(page).toHaveURL('https://saucelabs.com/');
+
+});
