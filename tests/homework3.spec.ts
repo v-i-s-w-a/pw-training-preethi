73681d5c5269ea8765d8b0097221de405764ba68
preethititus <preethititus003@gmail.com>
2026-09-09 13:32:54 +0530

    Add Firefox and WebKit browser projects (#12)
    * test: add firefox and webkit projects
    
    * Add Playwright CI workflow
    
    * Intentionally fail assertion for CI test
    
    * Fix SauceDemo title assertion

--- tests/homework3.spec.ts
+++ tests/homework3.spec.ts
@@ -5,7 +5,8 @@ test('Verify SauceDemo title and URL', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
 
 
-  await expect(page).toHaveTitle('Swag Labs');
+ await expect(page).toHaveTitle('Swag Labs');
+
 
 
   await expect(page).toHaveURL('https://www.saucedemo.com/');
